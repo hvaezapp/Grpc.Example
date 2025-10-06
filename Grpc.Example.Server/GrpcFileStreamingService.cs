79d042d2611dc2ad089bc8f1be1f29422b9f94ca
hassa <hassa@LAPTOP-U03CN6LR>
2025-10-06 16:31:11 +0330

    add grpc service for file streaming in server

---
+++ Grpc.Example.Server/GrpcFileStreamingService.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using Grpc.Example.Server.Protos;
+
+namespace Grpc.StreamingDemo.Api;
+
+public class GrpcFileStreamingService(IWebHostEnvironment environment, ILogger<GrpcFileStreamingService> logger)
+    : FileStreamingService.FileStreamingServiceBase
+{
+    public override async Task StreamFile(IAsyncStreamReader<FileContent> requestStream,
+        IServerStreamWriter<Result> responseStream, ServerCallContext context)
+    {
+        logger.LogInformation("Receiving File...");
+
+        if (!await requestStream.MoveNext())
+        {
+            logger.LogWarning("No file content received.");
+            return;
+        }
+
+        var fileName = environment.ContentRootPath + "/Files/" + requestStream.Current.FileName;
+        var fileSize = requestStream.Current.FileSize;
+
+        long totalBytesWritten = 0;
+
+        await using var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+
+        await fs.WriteAsync(requestStream.Current.File.ToArray());
+        totalBytesWritten += requestStream.Current.File.Length;
+
+        try
+        {
+            await foreach (var file in requestStream.ReadAllAsync())
+            {
+                await fs.WriteAsync(file.File.ToArray());
+
+                totalBytesWritten += file.File.Length;
+
+                await Task.Delay(20);
+
+                var streamResult = new Result { Percent = GetPercent(totalBytesWritten, fileSize) };
+
+                await responseStream.WriteAsync(streamResult);
+            }
+        }
+        finally
+        {
+            fs.Close();
+            logger.LogInformation("Saving File Completed");
+        }
+    }
+
+    private static double GetPercent(long written, double total)
+    {
+        if (total == 0) return 100;
+        return Math.Round(written / total * 100, 2);
+    }
+}
--- Grpc.Example.Server/Program.cs
+++ Grpc.Example.Server/Program.cs
@@ -1,5 +1,13 @@
+using Grpc.StreamingDemo.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddGrpc();
+
 var app = builder.Build();
+
+app.UseRouting();
+
+app.MapGrpcService<GrpcFileStreamingService>();
 
 app.Run();
